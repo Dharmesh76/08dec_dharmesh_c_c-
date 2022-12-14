0c16b8e1ecc2237da2ad4b6d41f6247d10c451d7
Dharmesh76 <120082639+Dharmesh76@users.noreply.github.com>
2022-12-14 14:49:57 +0530

    Add files via upload

---
+++ assignment/module1/module1.c
@@ -0,0 +1,10 @@
+//MODULE 1 (C language introduction)
+#include <stdio.h>
+void main()
+{
+    printf("my name is dharmesh balasara\n");
+    printf("my birth date 01 march,2004\n");
+    printf("my age is 18\n");
+    printf("my address:- bhaktinagar station plot2/18 kalyan society, 360002");
+    
+}
