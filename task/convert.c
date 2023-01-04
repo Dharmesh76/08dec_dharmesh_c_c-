252a454d6e775ea5f190c62568018988a2198f72
Dharmesh76 <120082639+Dharmesh76@users.noreply.github.com>
2023-01-04 20:40:20 +0530

    Add files via upload

---
+++ task/convert.c
@@ -0,0 +1,12 @@
+//wap to convert day into year
+#include<stdio.h>
+int main()
+{
+	float day;
+	float year;
+	printf("enter the number of days : ");
+	scanf("%f", &day);
+	year = day / 365;
+	printf("the number of years is : %f", year);
+	
+}
