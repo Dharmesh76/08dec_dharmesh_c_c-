bff070119e43d91ebe7820d67f54032231a6c8d9
Dharmesh76 <120082639+Dharmesh76@users.noreply.github.com>
2023-01-16 09:06:05 +0530

    Add files via upload

---
+++ task/check vowel character using if else.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("enter character : ");
+	scanf("%c",&ch);
+	if ((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+  {
+	  
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+	printf("eneter character is vowel");}
+	else if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+	printf("eneter character is vowel");}
+	else{printf("Enter character is Consonant");
+	}
+	
+	
+  }
+    else 
+	{printf("%c is not alphabet character",ch);}
+	
+	
+}
